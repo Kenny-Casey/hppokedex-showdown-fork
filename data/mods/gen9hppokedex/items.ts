0ab7b8151df40a6032e9b6c08c8f218c4979fab1
Kenny-Casey <kac9920@g.rit.edu>
2026-06-10 13:53:57 -0400

    Added the mega stones to the formats item list so hopefully they will show up as legal

---
+++ data/mods/gen9hppokedex/items.ts
@@ -0,0 +1,23 @@
+export const Items: import('../../../sim/dex-items').ModdedItemDataTable = {
+    baasphyxite: {
+        inherit: true,
+        isNonstandard: "HPPokedex",
+    },
+    reelingerite: {
+        inherit: true,
+        isNonstandard: "HPPokedex",
+    },
+    sablenite: {
+        inherit: true,
+        isNonstandard: null,
+    },
+    sableniteheroguea: {
+        inherit: true,
+        isNonstandard: "HPPokedex"
+    },
+    styrafimite: {
+        inherit: true,
+        isNonstandard: "HPPokedex"
+    },
+
+};
