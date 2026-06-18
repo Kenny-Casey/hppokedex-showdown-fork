6f6a2b1f5e780440f35bd4bc90636f58b30badfa
Kenny-Casey <kac9920@g.rit.edu>
2026-06-17 20:50:50 -0400

    Fixed eviolite Grapploct

--- data/mods/gen9hppokedex/items.ts
+++ data/mods/gen9hppokedex/items.ts
@@ -3,6 +3,19 @@ export const Items: import('../../../sim/dex-items').ModdedItemDataTable = {
         inherit: true,
         isNonstandard: null,
     },
+    eviolite: {
+		inherit: true,
+		onModifyDef(def, pokemon) {
+			if (pokemon.baseSpecies.nfe || pokemon.baseSpecies.id === 'grapploct') {
+				return this.chainModify(1.5);
+			}
+		},
+		onModifySpD(spd, pokemon) {
+			if (pokemon.baseSpecies.nfe || pokemon.baseSpecies.id === 'grapploct') {
+				return this.chainModify(1.5);
+			}
+		},
+	},
     reelingerite: {
         inherit: true,
         isNonstandard: null,
